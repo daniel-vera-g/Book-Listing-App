ab2d5230fc71d919df549bac204a142953fb504e
DanielVG <danielveragi@gmail.com>
2017-08-02 08:40:55 +0200

    Added a method to read the Input stream and convert i it a JSON response in a String

--- Book-Listing-App/app/src/main/java/com/example/android/book_listing_app/QueryUtils.java
+++ Book-Listing-App/app/src/main/java/com/example/android/book_listing_app/QueryUtils.java
@@ -6,11 +6,14 @@ package com.example.android.book_listing_app;
 
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 /** Helper methods to receive the information requested from the Google Books API*/
 public class QueryUtils {
@@ -88,6 +91,33 @@ public class QueryUtils {
             return jsonResponse;
         }
 
-
     }
+
+    /**
+     * convert the input stream into a string
+     * and return the JSON response from the server
+     * */
+    private static String readFromInputStream(InputStream inputStream) throws IOException{
+        //create new StringBuilder Object
+        StringBuilder output = new StringBuilder();
+
+        //if the inputStream is positive build up the Json String
+        if (inputStream != null){
+            //create InputStream reader Object
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            //create a new Buffered reader Object to read the Input stream
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            //declare new String variable and assign the value of the read line from the buffered reader
+            String line = bufferedReader.readLine();
+            //loop to build the String with the help of the string builder and keep reading new lines from the input stream as long as line is not null
+            while (line != null){
+                output.append(line);
+                line = bufferedReader.readLine();
+            }
+        }
+        //return the StringBuilder Object and convert it to string
+        return output.toString();
+    }
+
+    
 }
