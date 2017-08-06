bd166657431d39e1dd4e677ef0764c1a5f8b9a08
DanielVG <danielveragi@gmail.com>
2017-08-06 09:48:15 +0200

    Added the Textviews in the Adapter to set the Inforation in the Listview

--- Book-Listing-App/app/src/main/java/com/example/android/book_listing_app/BookAdapter.java
+++ Book-Listing-App/app/src/main/java/com/example/android/book_listing_app/BookAdapter.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -47,6 +48,32 @@ public class BookAdapter extends ArrayAdapter<Book> {
         }
         //find Book at the given position at the list of Books
         Book currentBook = getItem(position);
+
+        //find Textview with the view ID Title
+        TextView title = (TextView) listItemView.findViewById(R.id.bookTitle);
+        //Display the Title of the current Book in that Textview
+        title.setText(currentBook.getmTitle());
+
+        //find Textview with the view ID subtitle
+        TextView subtitle = (TextView) listItemView.findViewById(R.id.subtitle);
+        //Display the subtitle of the current Book in that Textview
+        subtitle.setText(currentBook.getmSubtitle());
+
+        //find Textview with the view ID author
+        TextView author = (TextView) listItemView.findViewById(R.id.authors);
+        //Display the author of the current Book in that Textview
+        author.setText(currentBook.getmAuthor());
+
+        //find Textview with the view ID publishedDate
+        TextView publishedDate = (TextView) listItemView.findViewById(R.id.publishedDate);
+        //Display the published date of the current Book in that Textview
+        publishedDate.setText(currentBook.getmAuthor());
+
+        //find Textview with the view ID numberPages
+        TextView numberOfPages = (TextView) listItemView.findViewById(R.id.pageCount);
+        //Display the number of pages of the current Book in that Textview
+        numberOfPages.setText(currentBook.getmNumberOfPages());
+
         return listItemView;
     }
 }
