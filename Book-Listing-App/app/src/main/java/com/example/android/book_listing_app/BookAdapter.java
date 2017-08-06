882b674ac5b1e1d1e7df7f76a5ea00a6276b464b
DanielVG <danielveragi@gmail.com>
2017-08-06 08:14:54 +0200

    Implemented some methods in the CustomAdapter

--- Book-Listing-App/app/src/main/java/com/example/android/book_listing_app/BookAdapter.java
+++ Book-Listing-App/app/src/main/java/com/example/android/book_listing_app/BookAdapter.java
@@ -4,8 +4,39 @@ package com.example.android.book_listing_app;
  * Created by DanielVG on 06.08.17.
  */
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
 /**
  * Knows how to create a List item layout for each Book in the data Source
  */
-public class BookAdapter {
+public class BookAdapter extends ArrayAdapter<Book> {
+
+    /**
+     * Constructs a new Earthquake Adapter
+     * @param context of the app
+     * @param books is the list of Books which is the data Source for the App
+     */
+    public BookAdapter(Context context, List<Book> books){
+        super(context, 0, books);
+    }
+
+    /**
+     *
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return List item view that displays information about the given Book Book in the list of Books
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
 }
--- Book-Listing-App/app/src/main/java/com/example/android/book_listing_app/MainActivity.java
+++ Book-Listing-App/app/src/main/java/com/example/android/book_listing_app/MainActivity.java
@@ -12,8 +12,13 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static android.R.string.no;
@@ -28,12 +33,41 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public static final int OPERATION_SEARCH_LOADER = 1;
     //url to make the request to
     public static final String REQUEST_URL = "https://www.googleapis.com/books/v1/volumes?q=android&maxResults=1";
+    //Adapter for the list of Books
+    public BookAdapter mAdapter;
+    //empty Textview to be displayed when there is no Internet connection
+    public TextView emptyTextView;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.booklist_activity);
+
+        //find a reference to the Listview in the layout
+        ListView bookListView = (ListView) findViewById(R.id.list);
+
+        //TODO: define the emptytextView & set ot to the Listview
+
+        //create new Adapter that takes an empty list of Books as Input
+        mAdapter = new BookAdapter(this, new ArrayList<Book>());
+
+        //set the adapter to the Listview
+        //This leads to the population of the user Interface
+        bookListView.setAdapter(mAdapter);
+
+        //set an itemOnClick Listener to when the user push the button
+        //This leads to a request to the browser with the query and  list ob Books as a response
+        bookListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //find the Button in the Layout
+                Button requestButon = (Button) findViewById(R.id.searchButton);
+
+                //TODO: Start the request to the server
+            }
+        });
 
         //make reference of the connectivity manager so I can check the netwrok status
         ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
