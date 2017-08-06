0f503b97457ace9a4cd828476b0e0f78bfce518a
DanielVG <danielveragi@gmail.com>
2017-08-06 09:26:13 +0200

    Created the basic design for the List Item xml file for a single Book in the List.

--- Book-Listing-App/app/src/main/java/com/example/android/book_listing_app/BookAdapter.java
+++ Book-Listing-App/app/src/main/java/com/example/android/book_listing_app/BookAdapter.java
@@ -7,6 +7,7 @@ package com.example.android.book_listing_app;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -37,6 +38,15 @@ public class BookAdapter extends ArrayAdapter<Book> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        //check if an existing ListItemView is there to reuse
+        //if not create one
+        View listItemView = convertView;
+        if (listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.booklist_activity, parent, false);
+        }
+        //find Book at the given position at the list of Books
+        Book currentBook = getItem(position);
+        return listItemView;
     }
 }
